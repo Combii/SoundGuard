9d4244d3d4ab7d4a1cbc6d52291413a78193b828
Combii <davidstovle@gmail.com>
2020-10-31 11:08:37 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-//This is test file
+const express = require('express')
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
