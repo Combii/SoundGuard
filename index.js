af40216eb40df16557f70476f00500e631314a4a
Combii <davidstovle@gmail.com>
2020-11-17 10:05:48 +0100

    Added get request

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const request = require('request')
+
 const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
+
+  request({
+    url: 'https://iotnet.cibicom.dk/1/nwk/app/BE7A133E/device/A8610A3130197313/last-data',
+    headers: {
+       'Authorization': 'Bearer AAABXg7P_8gYHhwLavwtpHORwZAXcu_st5EI7bTcrpi9raggY'
+    },
+    rejectUnauthorized: false
+  }, function(err, res) {
+        if(err) {
+          console.error(err);
+        } else {
+          console.log(res.body);
+        }
+  
+  });
+
+
   res.send('Hello World!')
 });
 
