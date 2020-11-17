e4dab86926b01582dd39470c515b25f2d47a932d
Combii <davidstovle@gmail.com>
2020-11-17 14:52:03 +0100

    Working

--- SoundGuard-Angular/soundGuardApp/src/app/app-routing.module.ts
+++ SoundGuard-Angular/soundGuardApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SoundDataComponent } from './sound-data/sound-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SoundDataComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
