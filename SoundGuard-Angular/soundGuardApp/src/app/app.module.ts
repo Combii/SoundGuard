ea8d96a998bdc9189e2f5c6cf50ab462ff2a6cf3
Combii <davidstovle@gmail.com>
2020-11-18 08:28:02 +0100

    .

--- SoundGuard-Angular/soundGuardApp/src/app/app.module.ts
+++ SoundGuard-Angular/soundGuardApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +13,11 @@ import { SoundDataComponent } from './sound-data/sound-data.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- SoundGuard-Angular/soundGuardApp/src/app/sound-data/sound-data.component.ts
+++ SoundGuard-Angular/soundGuardApp/src/app/sound-data/sound-data.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoundDataComponent implements OnInit {
 
-  constructor() { }
+  url = `https://iotnet.cibicom.dk/1/nwk/app/BE7A133E/device/A8610A3130197313/last-data`;
+
+  value = 'test';
+
+  constructor(private http: HttpClient) {
+
+    const headers = new HttpHeaders()
+        .set('content-type', 'application/json')
+        .set('Authorization', `Bearer AAABXg7P_8gYHhwLavwtpHORwZAXcu_st5EI7bTcrpi9raggY`);
+
+    this.http.get(this.url, {headers}).subscribe(responseData => {
+      console.log(responseData);
+    });
+   }
 
   ngOnInit(): void {
   }
